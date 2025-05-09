ebeefe98091edbecf5a6103eccba0914dcbfb962
minsu454 <kminsu454@gmail.com>
2025-05-09 16:25:04 +0900

    [Update] 라인 중복 연결 불가능하게 구현

--- WaterGrid/Assets/Dev/Scripts/Object/LinkedObject.cs
+++ WaterGrid/Assets/Dev/Scripts/Object/LinkedObject.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(SpriteRenderer))]
@@ -26,19 +25,19 @@ public abstract class LinkedObject : MonoBehaviour
     private int curConnectCost = 0;                             //현재 연결 비용
     [SerializeField] private int maxConnectCost;                //최대 연결 비용
 
-    private List<Line> _lineList = new List<Line>();            //연결된 라인 
-    private Line line;
-
-    public event Action<int, Vector2> lineUpdateEvent;               //라인위치 업데이트 이벤트
+    public event Action<int, Vector2> lineUpdateEvent;          //라인위치 업데이트 이벤트
 
     [Header("Text")]
     [SerializeField] private bool isNeedText;                   //텍스트 필요한지 확인 값
     [SerializeField] private TextMeshPro costText;              //코스트 텍스트
 
+    private Line tempLine;                                                                      //임시 라인
+    private Dictionary<LinkedObject, Line> _lineDict = new Dictionary<LinkedObject, Line>();    //연결된 라인 
+
     public void Reset()
     {
-        lineUpdateEvent -= line.OnUpdate;
-        line = null;
+        lineUpdateEvent -= tempLine.OnUpdate;
+        tempLine = null;
     }
 
     private void Update()
@@ -51,11 +50,11 @@ public abstract class LinkedObject : MonoBehaviour
     /// </summary>
     public void CreateLine()
     {
-        line = GameManager.instance.lineObjectPool.GetObject();
+        tempLine = GameManager.instance.lineObjectPool.GetObject();
 
-        line.Link(transform);
-        lineUpdateEvent += line.OnUpdate;
-        line.gameObject.SetActive(true);
+        tempLine.Link(transform);
+        lineUpdateEvent += tempLine.OnUpdate;
+        tempLine.gameObject.SetActive(true);
     }
 
     /// <summary>
@@ -63,10 +62,10 @@ public abstract class LinkedObject : MonoBehaviour
     /// </summary>
     public void ConnectLine(LinkedObject linkedObject)
     {
-        if (this != linkedObject && linkedObject.ConnectType.HasFlag(myType) && connectType.HasFlag(linkedObject.Type))
+        if (this != linkedObject && _lineDict.ContainsKey(linkedObject) is false && CanConnect(linkedObject))
         {
-            linkedObject.AddLine(line);
-            AddLine(line);
+            linkedObject.AddLine(this, tempLine);
+            AddLine(linkedObject, tempLine);
 
             lineUpdateEvent?.Invoke(1, linkedObject.transform.position);
             OnConnectLine();
@@ -84,15 +83,23 @@ public abstract class LinkedObject : MonoBehaviour
     /// </summary>
     public void DeleteLine()
     {
-        line.Unlink();
+        tempLine.Unlink();
     }
 
     /// <summary>
     /// 라인을 오브젝트에 추가해주는 함수
     /// </summary>
-    private void AddLine(Line line)
+    private void AddLine(LinkedObject linkObj, Line line)
     {
-        _lineList.Add(line);
+        _lineDict.Add(linkObj, line);
+    }
+
+    /// <summary>
+    /// 해당 오브젝트와 연결이 가능한지 알려주는 함수
+    /// </summary>
+    private bool CanConnect(LinkedObject linkedObject)
+    {
+        return linkedObject.ConnectType.HasFlag(myType) && connectType.HasFlag(linkedObject.Type);
     }
 
     /// <summary>
