67ad65eb9b0267541052072ffea1731ee2468f51
minsu454 <kminsu454@gmail.com>
2025-05-21 16:57:22 +0900

    [Update] 25.5.17 build

--- WaterGrid/Assets/Dev/Scripts/Scene/InGameLoader.cs
+++ WaterGrid/Assets/Dev/Scripts/Scene/InGameLoader.cs
@@ -1,7 +1,6 @@
 using Common.Assets;
 using Cysharp.Threading.Tasks;
 using System;
-using UnityEditor.Build.Content;
 using UnityEngine;
 
 public class InGameLoader : BaseSceneLoader<InGameLoader>
